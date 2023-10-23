41272c46df8bf995a41b8a2cc790634c1533f20d
joandercim <joacim@josiasmusik.se>
2023-10-23 16:59:55 +0200

    animatePresence

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -6,34 +6,34 @@ function FeedbackList({ feedback, handleDelete }) {
   if (!feedback || feedback.length === 0) {
     return <p>No Feedback Yet</p>;
   }
-  // return (
-  //     <AnimatePresence>
-  //   <div className="feedback-list">
-  //       {feedback.map((item) => (
-  //         <motion.div
-  //           key={item.id}
-  //           initial={{ opacity: 0 }}
-  //           animate={{ opacity: 1 }}
-  //           exit={{ opacity: 1 }}
-  //         >
-  //           <FeedbackItem
-  //             key={item.id}
-  //             item={item}
-  //             handleDelete={handleDelete}
-  //           />
-  //         </motion.div>
-  //       ))}
-  //     </div>
-  //   </AnimatePresence>
-  // );
-
-    return (
-      <div className="feedback-list">
-        {feedback.map(item => {
-          return <FeedbackItem key={item.id} item={item} handleDelete={handleDelete}/>
-  })}
+  return (
+    <div className="feedback-list">
+        <AnimatePresence>
+        {feedback.map((item) => (
+          <motion.div
+            key={item.id}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+          >
+            <FeedbackItem
+              key={item.id}
+              item={item}
+              handleDelete={handleDelete}
+            />
+          </motion.div>
+        ))}
+    </AnimatePresence>
       </div>
-    )
+  );
+
+  //   return (
+  //     <div className="feedback-list">
+  //       {feedback.map(item => {
+  //         return <FeedbackItem key={item.id} item={item} handleDelete={handleDelete}/>
+  // })}
+  //     </div>
+  //   )
 }
 
 FeedbackList.propTypes = {
